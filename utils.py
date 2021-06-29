5dff4815a70837af9408debaa80ce9579ae95090
Robert Miller <bert.c.miller@gmail.com>
2021-06-29 14:46:50 -0400

    Create two functions to check for signatures, one for single signatures, one for multiple

--- utils.py
+++ utils.py
@@ -1,21 +1,42 @@
 from hexbytes.main import HexBytes
 
-def check_call_for_signature(call, signatures):
+def check_call_for_signatures(call, signatures):
+    ## used for checking a call for an array of signatures
+
     if (call['action']['input'] == None):
         return False
     
     ## By default set this to False
     signature_present_boolean = False
-
+    
     ## Iterate over all signatures, and if our call matches any of them set it to True
     for signature in signatures:
+        # print(signature)
         # print("Desired signature:", str(signature))
         # print("Actual", HexBytes(call['action']['input']))
-        
-        if HexBytes(call['action']['input']).startswith((signature)):
+
+        if HexBytes(call['action']['input']).startswith(signature):
             ## Note that we are turning the input into hex bytes here, which seems to be fine
             ## Working with strings was doing weird things
-            print("hit")
             signature_present_boolean = True
 
     return signature_present_boolean
+
+
+def check_call_for_signature(call, signature):
+    ## used for checking a single signature, not an array
+    
+    if (call['action']['input'] == None):
+        return False
+    
+    ## If our call matches the signature return true
+
+    if HexBytes(call['action']['input']).startswith(signature):
+        ## Note that we are turning the input into hex bytes here, which seems to be fine
+        ## Working with strings was doing weird things
+        return True
+
+
+def sub_call_match(call, trace_call_address):
+    if trace_call_address == call['traceAddress'][0:len(trace_call_address)]:
+        return True

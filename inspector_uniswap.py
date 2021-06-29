890cb2f2a49b1bf570094ac44bf98765e9b00cfa
Robert Miller <bert.c.miller@gmail.com>
2021-06-29 14:48:02 -0400

    Identify trades on routers, direct to pairs, and pre-flights. Also change how selectors are generated slightly.

--- inspector_uniswap.py
+++ inspector_uniswap.py
@@ -7,6 +7,7 @@ import utils
 config = configparser.ConfigParser()
 config.read('./utils/config.ini')
 
+## Load up the ABIs and addresses to make them easier to handle
 uniswap_router_abi = json.loads(config['ABI']['UniswapV2Router'])
 uniswap_router_address = (config['ADDRESSES']['UniswapV2Router'])
 sushiswap_router_address = (config['ADDRESSES']['SushiswapV2Router'])
@@ -22,10 +23,13 @@ class UniswapInspector:
         self.uniswap_router_trade_signatures = self.get_router_signatures()
 
         self.uniswap_v2_pair_contract = self.w3.eth.contract(abi=uniswap_pair_abi)
-        self.uniswap_v2_pair_swap_signatures = self.uniswap_v2_pair_contract.functions.swap(0, 0, uniswap_router_address, "").selector ## Note the address here doesn't matter, but it must be filled out
-        self.uniswap_v2_pair_reserves_signatures = self.uniswap_v2_pair_contract.functions.getReserves().selector ## Called "checksigs" in mev-inpsect.ts
+        self.uniswap_v2_pair_swap_signature = Web3.sha3(text='swap(uint256,uint256,address,bytes)')[0:4]
+        # Alternative returned as a string, not hexbytes: (self.uniswap_v2_pair_contract.functions.swap(0, 0, uniswap_router_address, "").selector) ## Note the address here doesn't matter, but it must be filled out
+        self.uniswap_v2_pair_reserves_signature = Web3.sha3(text='getReserves()')[0:4]
+         # Alternative returned as a string, not hexbytes: self.uniswap_v2_pair_contract.functions.getReserves().selector ## Called "checksigs" in mev-inpsect.ts
 
         print("Built Uniswap inspector")
+    
     def get_trading_functions(self):
         ## Gets all functions used for swapping
         result = []
@@ -70,12 +74,143 @@ class UniswapInspector:
         return result
 
     def inspect(self, calls):
+        # print("Inspecting calls for Uniswappy trades")
         result = []
 
+        ## Identifies trades that go directly to the Uniswap router
+        for trade_call in self.get_router_trade_calls(calls):
+            sub_calls_of_trade = []
+            for call in calls:
+                if call['transactionHash'] == trade_call['transactionHash'] and utils.sub_call_match(call, trade_call['traceAddress']):
+                    ## The intention here is to get the sub calls that correspond to a trade.
+                    sub_calls_of_trade.append(call)
+            
+            if len(sub_calls_of_trade) == 0:
+                ## Consider a Uniswap router call-tree calling back into Uniswap Router. We cull parts of the call tree as we process it
+                print("Removed a recursive call")
+                continue
+            
+            ## We know this is a trade on Uniswap, now let's decode it to understand its inputs
+            trade_inputs = self.uniswap_v2_router_contract.decode_function_input(data = trade_call['action']['input'])
+            args = trade_inputs[1]
+            to = args['to']
+            path = args['path']
+            source_token = path[0].lower()
+            dest_token = path[len(path) - 1].lower()
+
+            ## This can probably be turned into a terniary but I can't remember how to do that and I'm on a plane
+            provider = ''
+            if trade_call['action']['to'] == uniswap_router_address.lower():
+                provider = uniswap_router_address
+            elif trade_call['action']['to'] == sushiswap_router_address.lower():
+                provider = sushiswap_router_address
+
+            ## Create a structured object for the results
+            action = {
+                'provider': provider,
+                'action_type': 'tbd',  #     type: ACTION_TYPE.TRADE,
+                'action_calls': sub_calls_of_trade,
+                'transaction_hash': trade_call['transactionHash'],
+                'subcall': trade_call,
+                'status': 'tbd'  #     status: tradeCall.reverted ? ACTION_STATUS.REVERTED : ACTION_STATUS.SUCCESS,
+            }
+
+            ## TODO:
+            ## implement "action" data structure
+            ## implement the "token tracker" from inspect-ts
+            ## get action_types
+            ## get trade status (did it revert?)
+            ## get what the result of the trade was, currently this only *identifies* a trade
+
+            result.append(action)
+
+        ## Identified trades that go directly to pairs
+        for pair_trade_call in self.get_pair_trade_calls(calls):
+            sub_calls_of_pair_trade = []
+
+            for call in calls:
+                if call['transactionHash'] == pair_trade_call['transactionHash'] and utils.sub_call_match(call, pair_trade_call['traceAddress']):
+                    ## The intention here is to get the sub calls that correspond to a trade.
+                    sub_calls_of_pair_trade.append(call)
+
+            action = {
+                'provider': uniswap_router_address, # TBD
+                'action_type': 'tbd',  #     type: ACTION_TYPE.TRADE, also TBD
+                'action_calls': sub_calls_of_pair_trade,
+                'transaction_hash': pair_trade_call['transactionHash'],
+                'subcall': pair_trade_call,
+                'status': 'tbd'  #     status: tradeCall.reverted ? ACTION_STATUS.REVERTED : ACTION_STATUS.SUCCESS,
+            }
+
+            ## TODO:
+            ## implement "action" data structure
+            ## implement the "token tracker" from inspect-ts
+            ## get action_types
+            ## get trade status (did it revert?)
+            ## get what the result of the trade was, currently this only *identifies* a trade
+
+            result.append(action)
+        
+        ## Identifies "pre-flights" that check reserves before trading
+        for check_reserves_call in self.get_pair_reserves_calls(calls):
+            sub_calls_of_check_reserves_call = []
+
+            for call in calls:
+                if call['transactionHash'] == check_reserves_call['transactionHash'] and utils.sub_call_match(call, check_reserves_call['traceAddress']):
+                    ## The intention here is to get the sub calls that correspond to a trade.
+                    sub_calls_of_check_reserves_call.append(call)
+
+            action = {
+                'provider': uniswap_router_address, # TBD
+                'action_type': 'tbd',  #     type: ACTION_TYPE.TRADE, also TBD
+                'action_calls': sub_calls_of_check_reserves_call,
+                'transaction_hash': check_reserves_call['transactionHash'],
+                'subcall': check_reserves_call,
+                'status': 'tbd'  #     ACTION_STATUS.CHECKED,
+            }
+
+            result.append(action)
+        
+        return result
+
+   
+    def get_router_trade_calls(self, calls):
+        ## Identifies the calls match the desired signatures (PLURAL), which in this case are the v2 routers trade functions, as well as aren't create/create2/suicides
+        
         trade_calls = []
+        for call in self.filter_call_types(calls):
+            if (call['action']['to'] == uniswap_router_address.lower() or call['action']['to'] == sushiswap_router_address.lower()) and utils.check_call_for_signatures(call, self.uniswap_router_trade_signatures):
+                trade_calls.append(call)
+
+        return trade_calls
+
+    
+    def get_pair_trade_calls(self, calls):
+        ## Identifies the calls match the desired signature (NOT PLURAL), which in this case is the v2 pair swap function, as well as aren't create/create2/suicides
+        
+        trade_calls = []
+        for call in self.filter_call_types(calls):
+            if (utils.check_call_for_signature(call, self.uniswap_v2_pair_swap_signature)):
+                trade_calls.append(call)
+
+        return trade_calls
+    
+    def get_pair_reserves_calls(self, calls):
+        ## Identifies the calls match the desired signature (NOT PLURAL), which in this case is the v2 pair get reserves function, as well as aren't create/create2/suicides or delegateCalls
+
+        trade_calls = []
+        for call in self.filter_call_types(calls):
+            if (call['type'] != 'delegateCall' and utils.check_call_for_signature(call, self.uniswap_v2_pair_reserves_signature)):
+                trade_calls.append(call)
+
+        return trade_calls
+
+    def filter_call_types(self, calls):
+        # Filters out some calls that we don't want
+        result =[]
 
         for call in calls:
-            print('\n',call)
-            if (call['action']['to'] == uniswap_router_address.lower() or call['action']['to'] == sushiswap_router_address.lower()) and utils.check_call_for_signature(call, self.uniswap_router_trade_signatures):
-                # print("WIP, here is where there is a call that matches what we are looking for")
-                1 == 1
+            if (call['type']!= 'suicide' and call['type'] != 'create' and call['type'] != 'create2'):
+                result.append(call)
+
+        return result

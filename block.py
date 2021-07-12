f7b0c7244c6b8f3c613fabfb34daf5c015a8bfda
Luke Van Seters <lukevanseters@gmail.com>
2021-07-12 12:32:15 -0400

    Factor out fetching a block

--- block.py
+++ block.py
@@ -10,6 +10,67 @@ from schemas import Block, BlockCall, BlockCallType
 cache_directory = './cache'
 
 
+## Creates a block object, either from the cache or from the chain itself
+## Note that you need to pass in the provider, not the web3 wrapped provider object!
+## This is because only the provider allows you to make json rpc requests
+def createFromBlockNumber(block_number: int, base_provider) -> Block:
+    cache_path = _get_cache_path(block_number)
+
+    if (cache_path.is_file()):
+        print(
+            f'Cache for block {block_number} exists, ' \
+            'loading data from cache'
+        )
+
+        return Block.parse_file(cache_path)
+
+    else:
+        print(
+           f"Cache for block {block_number} did not exist, getting data"
+        )
+
+        w3 = Web3(base_provider)
+        block = fetch_block(w3, block_number)
+
+        cache_block(cache_path, block)
+
+        return block
+
+
+def fetch_block(w3, block_number: int) -> Block:
+    ## Get block data
+    block_data = w3.eth.get_block(block_number, True)
+
+    ## Get the block receipts
+    ## TODO: evaluate whether or not this is sufficient or if gas used needs to be converted to a proper big number.
+    ## In inspect-ts it needed to be converted
+    block_receipts_raw = base_provider.make_request(
+        "eth_getBlockReceipts", [block_number],
+    )
+
+    ## Trace the whole block, return those calls
+    block_calls_json = w3.parity.trace_block(block_number)
+    block_calls = [
+        BlockCall(**call_json)
+        for call_json in block_calls_json
+    ]
+
+    ## Get the logs
+    block_hash = (block_data.hash).hex()
+    block_logs = w3.eth.get_logs({'blockHash': block_hash})
+
+    transaction_hashes = get_transaction_hashes(block_calls)
+
+    return Block(
+        block_number=block_number,
+        data=block_data,
+        receipts=block_receipts_raw,
+        calls=block_calls,
+        logs=block_logs,
+        transaction_hashes=transaction_hashes,
+    )
+
+
 def get_transaction_hashes(calls: List[BlockCall]) -> List[str]:
     result = []
 
@@ -17,74 +78,15 @@ def get_transaction_hashes(calls: List[BlockCall]) -> List[str]:
         if call.type != BlockCallType.reward:
             if call.transaction_hash not in result:
                 result.append(call.transaction_hash)
-    
+
     return result
 
 
-def write_json(block: Block):
-    cache_path = _get_cache_path(block.block_number)
+def cache_block(cache_path: Path, block: Block):
     write_mode = "w" if cache_path.is_file() else "x"
 
     with open(cache_path, mode=write_mode) as cache_file:
-        cache_file.write(block.json(sort_keys=True, indent=4))
-
-
-## Creates a block object, either from the cache or from the chain itself
-## Note that you need to pass in the provider, not the web3 wrapped provider object!
-## This is because only the provider allows you to make json rpc requests
-def createFromBlockNumber(block_number: int, base_provider) -> Block:
-    cache_path = _get_cache_path(block_number)
-    
-    ## Check to see if the data already exists in the cache
-    ## if it exists load the data from cache
-    ## If not then get the data from the chain and save it to the cache
-    if (cache_path.is_file()):
-        print(
-            f'Cache for block {block_number} exists, ' \
-            'loading data from cache'
-        )
-
-        block = Block.parse_file(cache_path)
-        return block
-    else:
-        w3 = Web3(base_provider)
-        print(("Cache for block {block_number} did not exist, getting data").format(block_number=block_number))
-        
-        ## Get block data
-        block_data = w3.eth.get_block(block_number, True)
-        
-        ## Get the block receipts
-        ## TODO: evaluate whether or not this is sufficient or if gas used needs to be converted to a proper big number.
-        ## In inspect-ts it needed to be converted
-        block_receipts_raw = base_provider.make_request("eth_getBlockReceipts", [block_number])
-
-        ## Trace the whole block, return those calls
-        block_calls_json = w3.parity.trace_block(block_number)
-        block_calls = [
-            BlockCall(**call_json)
-            for call_json in block_calls_json
-        ]
-        
-        ## Get the logs
-        block_hash = (block_data.hash).hex()
-        block_logs = w3.eth.get_logs({'blockHash': block_hash})
-
-        transaction_hashes = get_transaction_hashes(block_calls)
-
-        ## Create a new object
-        block = Block(
-            block_number=block_number,
-            data=block_data,
-            receipts=block_receipts_raw,
-            calls=block_calls,
-            logs=block_logs,
-            transaction_hashes=transaction_hashes,
-        )
-        
-        ## Write the result to a JSON file for loading in the future
-        write_json(block)
-
-        return block
+        cache_file.write(block.json())
 
 
 def _get_cache_path(block_number: int) -> Path:

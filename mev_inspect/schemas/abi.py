b17c3d86a37fdef5389781e85ecda376335ae7c0
Luke Van Seters <lukevanseters@gmail.com>
2021-07-15 16:37:59 -0400

    Add functions for getting signatures and selectors

--- mev_inspect/schemas/abi.py
+++ mev_inspect/schemas/abi.py
@@ -2,7 +2,11 @@ from enum import Enum
 from typing import List, Union
 from typing_extensions import Literal
 
+from hexbytes import HexBytes
 from pydantic import BaseModel
+from web3 import Web3
+
+from .utils import CamelModel
 
 
 class ABIDescriptionType(str, Enum):
@@ -21,9 +25,8 @@ NON_FUNCTION_DESCRIPTION_TYPES = Union[
 ]
 
 
-class ABIDescriptionInput(BaseModel):
-    name: str
-    type: str
+class ABIDescriptionInput(CamelModel):
+    internal_type: str
 
 
 class ABIGenericDescription(BaseModel):
@@ -34,6 +37,15 @@ class ABIFunctionDescription(BaseModel):
     type: Literal[ABIDescriptionType.function]
     name: str
     inputs: List[ABIDescriptionInput]
+
+    def get_selector(self) -> HexBytes:
+        signature = self.get_signature()
+        return Web3.sha3(text=signature)[0:4]
+
+    def get_signature(self) -> str:
+        joined_input_types = ",".join(input.internal_type for input in self.inputs)
+
+        return f"{self.name}({joined_input_types})"
 
 
 ABIDescription = Union[ABIFunctionDescription, ABIGenericDescription]

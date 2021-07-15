89712e41bfab47710e625038c671539b8a8b4dad
Luke Van Seters <lukevanseters@gmail.com>
2021-07-15 16:28:40 -0400

    Break out the function description

--- mev_inspect/schemas/abi.py
+++ mev_inspect/schemas/abi.py
@@ -1,10 +1,11 @@
 from enum import Enum
-from typing import List, Optional
+from typing import List, Union
+from typing_extensions import Literal
 
 from pydantic import BaseModel
 
 
-class ABIDescriptionType(Enum):
+class ABIDescriptionType(str, Enum):
     function = "function"
     constructor = "constructor"
     fallback = "fallback"
@@ -12,15 +13,28 @@ class ABIDescriptionType(Enum):
     receive = "receive"
 
 
+NON_FUNCTION_DESCRIPTION_TYPES = Union[
+    Literal[ABIDescriptionType.constructor],
+    Literal[ABIDescriptionType.fallback],
+    Literal[ABIDescriptionType.event],
+    Literal[ABIDescriptionType.receive],
+]
+
+
 class ABIDescriptionInput(BaseModel):
     name: str
     type: str
 
 
-class ABIDescription(BaseModel):
-    type: ABIDescriptionType
-    name: Optional[str]
-    inputs: Optional[List[ABIDescriptionInput]]
+class ABIGenericDescription(BaseModel):
+    type: NON_FUNCTION_DESCRIPTION_TYPES
 
 
+class ABIFunctionDescription(BaseModel):
+    type: Literal[ABIDescriptionType.function]
+    name: str
+    inputs: List[ABIDescriptionInput]
+
+
+ABIDescription = Union[ABIFunctionDescription, ABIGenericDescription]
 ABI = List[ABIDescription]

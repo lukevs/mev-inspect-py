52f6f0244d5ec193ac1ab889196a3cf699e05111
Luke Van Seters <lukevanseters@gmail.com>
2021-07-27 16:44:00 -0400

    Add gas and value. Switch to numeric

--- alembic/versions/0660432b9840_create_classifications_table.py
+++ alembic/versions/0660432b9840_create_classifications_table.py
@@ -6,7 +6,6 @@ Create Date: 2021-07-23 20:08:42.016711
 
 """
 import sqlalchemy as sa
-from sqlalchemy.dialects.postgresql import JSON
 from alembic import op
 
 # revision identifiers, used by Alembic.
@@ -21,7 +20,7 @@ def upgrade():
         "classified_traces",
         sa.Column("classified_at", sa.TIMESTAMP, server_default=sa.func.now()),
         sa.Column("transaction_hash", sa.String(66), nullable=False),
-        sa.Column("block_number", sa.Integer, nullable=False),
+        sa.Column("block_number", sa.Numeric, nullable=False),
         sa.Column(
             "classification",
             sa.String(256),
@@ -33,9 +32,11 @@ def upgrade():
         sa.Column("abi_name", sa.String(1024), nullable=True),
         sa.Column("function_name", sa.String(2048), nullable=True),
         sa.Column("function_signature", sa.String(2048), nullable=True),
-        sa.Column("inputs", JSON, nullable=True),
+        sa.Column("inputs", sa.JSON, nullable=True),
         sa.Column("from_address", sa.String(256), nullable=True),
         sa.Column("to_address", sa.String(256), nullable=True),
+        sa.Column("gas", sa.Numeric, nullable=True),
+        sa.Column("value", sa.Numeric, nullable=True),
         sa.PrimaryKeyConstraint("transaction_hash", "trace_address"),
     )
 
